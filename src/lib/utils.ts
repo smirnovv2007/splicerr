b0467da9cdfdf755bb085c0a01f2897a9a94b6a4
Robert Kossessa <github@kosro.de>
2025-02-04 19:31:03 +0100

    FIX: Key selection & display + version bump

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,11 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+import type { ChordType, Key } from "./splice/types"
 
 export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
+    return twMerge(clsx(inputs))
 }
 
-export const formatKey = (key: string) => {
-	const upper = key.toUpperCase()
-	return upper + (key != upper ? " min" : "")
+export const formatKey = (key: Key, chord_type: ChordType | null = null) => {
+    return key.toUpperCase() + (chord_type ? ` ${chord_type.substring(0,3)}` : "")
 }
