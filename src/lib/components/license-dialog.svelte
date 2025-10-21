c9124ce29a1af1bfa2b69ad28695e46a832a38d9
Vladimir <34893489+smirnovv2007@users.noreply.github.com>
2025-10-22 00:16:10 +0300

    Replace settings dialog with license activation dialog

--- src/lib/components/license-dialog.svelte
+++ src/lib/components/license-dialog.svelte
@@ -1,146 +1,56 @@
 <script lang="ts">
     import * as Dialog from "$lib/components/ui/dialog"
-    import Settings from "lucide-svelte/icons/settings"
     import { buttonVariants } from "$lib/components/ui/button/index.js"
     import { cn } from "$lib/utils"
-    import ExternalLink from "$lib/components/external-link.svelte"
     import Input from "$lib/components/ui/input/input.svelte"
     import Button from "$lib/components/ui/button/button.svelte"
-    import FolderOpen from "lucide-svelte/icons/folder-open"
+    import Check from "lucide-svelte/icons/check"
     import TriangleAlert from "lucide-svelte/icons/triangle-alert"
-    import Undo2 from "lucide-svelte/icons/undo-2"
     import Label from "$lib/components/ui/label/label.svelte"
     import {
         config,
-        isSamplesDirValid,
+        isActivationKeyValid,
         saveConfig,
-        settingsDialog,
-        updateTheme,
+        licenseDialog,
+        activate
     } from "$lib/shared/config.svelte"
-    import Slider from "$lib/components/ui/slider/slider.svelte"
-    import { open as openDialog } from "@tauri-apps/plugin-dialog"
-    import ThemeSelect from "./theme-select.svelte"
-    import Switch from "$lib/components/ui/switch/switch.svelte"
-
-    let flashbangAudio = $state<HTMLAudioElement>(null!)
 </script>
 
-<Dialog.Root bind:open={settingsDialog.open}>
-    <Dialog.Trigger
-        class={cn(
-            buttonVariants({ variant: "outline", size: "icon" }),
-            "text-muted-foreground flex-shrink-0"
-        )}><Settings /></Dialog.Trigger
-    >
+<Dialog.Root bind:open={licenseDialog.open}>
     <Dialog.Content>
         <Dialog.Header>
-            <Dialog.Title>Settings</Dialog.Title>
+            <Dialog.Title>Activation</Dialog.Title>
         </Dialog.Header>
         <div class="flex flex-col gap-4 py-4">
             <div class="flex flex-col gap-2">
-                <Label for="fileInput">Samples Directory</Label>
+                <Label for="fileInput">Activation key</Label>
                 <p class="text-muted-foreground text-sm">
-                    This is where Splice will place the Packs & Samples you
-                    download.
+                    Enter your activation key here
                 </p>
                 <div class="flex gap-2">
                     <Input
                         type="text"
-                        class={isSamplesDirValid()
+                        class={isActivationKeyValid()
                             ? ""
                             : "border-warn focus-visible:border-warn focus-visible:outline-warn"}
-                        placeholder="e.g. .../Documents/Samples/Splice"
-                        bind:value={config.samples_dir}
+                        bind:value={config.activation_key}
                         oninput={saveConfig}
                     />
                     <Button
                         class="flex-shrink-0 text-accent-foreground"
                         size="icon"
                         variant="outline"
-                        onclick={() => {
-                            openDialog({
-                                multiple: false,
-                                directory: true,
-                            }).then((path) => {
-                                if (path) {
-                                    config.samples_dir = path
-                                    saveConfig()
-                                }
-                            })
-                        }}><FolderOpen /></Button
+                        onclick={activate}><Check /></Button
                     >
                 </div>
                 <div
                     class={cn(
                         "flex gap-2 items-center text-warn text-sm",
-                        isSamplesDirValid() && "opacity-0"
+                        isActivationKeyValid() && "opacity-0"
                     )}
                 >
                     <TriangleAlert size="16" />
-                    Enter a valid path to an existing directory.
-                </div>
-            </div>
-            <div class="flex flex-col gap-2">
-                <Label for="themeSelect">Theme</Label>
-                <div class="flex gap-2">
-                    <ThemeSelect
-                        bind:value={config.ui_theme}
-                        onselect={() => {
-                            if (config.ui_theme == "light")
-                                flashbangAudio.play()
-                            updateTheme()
-                            saveConfig()
-                        }}
-                    />
-                    <audio
-                        bind:this={flashbangAudio}
-                        src="/flashbang.mp3"
-                        preload="auto"
-                    ></audio>
-                </div>
-            </div>
-            <div class="flex flex-col gap-2">
-                <Label for="uiScaleSlider">UI Scale</Label>
-                <div class="flex gap-4">
-                    <Slider
-                        id="uiScaleSlider"
-                        min={0.5}
-                        max={2}
-                        step={0.1}
-                        type="single"
-                        bind:value={config.ui_scale}
-                        onValueCommit={saveConfig}
-                    />
-                    <Button
-                        class="flex-shrink-0 text-accent-foreground"
-                        size="icon"
-                        variant="outline"
-                        onclick={() => {
-                            config.ui_scale = 1
-                            saveConfig()
-                        }}
-                    >
-                        <Undo2 />
-                    </Button>
-                </div>
-            </div>
-            <div class="flex flex-col gap-2">
-                <Label for="repeatAudioToggle">Repeat Audio</Label>
-                <p class="text-muted-foreground text-sm">
-                    When enabled, audio will repeat after finishing.
-                </p>
-                <div class="flex items-center gap-2">
-                    <Switch
-                        id="repeatAudioToggle"
-                        bind:checked={config.repeat_audio}
-                        onchange={() => {
-                            config.repeat_audio = !config.repeat_audio
-                            saveConfig()
-                        }}
-                    />
-                    <Label for="repeatAudioToggle" class="cursor-pointer">
-                        {config.repeat_audio ? "Enabled" : "Disabled"}
-                    </Label>
+                    Invalid activation code
                 </div>
             </div>
         </div>
