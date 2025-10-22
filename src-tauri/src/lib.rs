532d30aecd994c9f0054e0a4a174e60450726d2a
Vladimir <34893489+smirnovv2007@users.noreply.github.com>
2025-10-22 19:18:44 +0300

    Remove devtools plugin from Tauri setup
    Removed the devtools plugin from the Tauri application.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,6 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_devtools::init())
         .plugin(tauri_plugin_window_state::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
